6fdbacfd880d4d17692b49ef5b01706eb721891c
Che-Wei Lin <linton.tw@gmail.com>
2015-06-27 16:34:08 +0800

    Fixed for coding style

--- dcarParserEx.js
+++ dcarParserEx.js
@@ -2,20 +2,20 @@
 // Author: John-Lin(https://github.com/John-Lin), lockys(https://github.com/lockys)
 // For all forum name refer to:
 // https://github.com/lockys/0card/blob/master/dacrdAPI.md#forum-list
-var dcardPaser = require('./lib/dard-parser');
-dcardPaser = new dcardPaser();
+var DcardPaser = require('./lib/dard-parser');
+dcardPaser = new DcardPaser();
 /**
  * Get Dcard Posts ID by forum name and forum page number
  * @param {String} forum name
  * @param {Number} forum page number
  * @return {Number} post ID Number
  */
-dcardPaser.getPostIdByForum ('sex', 3, function (err, postID) {
-    if (!err) {
-      console.log('Post id: ' + postID);
-    } else {
-      console.log(err);
-    }
+dcardPaser.getPostIdByForum ('sex', 3, function(err, postID) {
+  if (!err) {
+    console.log('Post id: ' + postID);
+  } else {
+    console.log(err);
+  }
 });
 
 /**
@@ -23,7 +23,7 @@ dcardPaser.getPostIdByForum ('sex', 3, function (err, postID) {
  * @param {Number} post id
  * @return {String} title, content of post.
  */
-dcardPaser.getContentByPostID(328484, function(err, post){
+dcardPaser.getContentByPostID(328484, function(err, post) {
   if (!err) {
     console.log('title: ' + post.title);
     console.log('content: ' + post.content);
