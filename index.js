ceafa2fb228af478dcf3cc64b5fd6c12e48807a9
lockys <locky4567@gmail.com>
2015-06-28 09:26:40 +0800

    fix cs & get post by time range and sort by time and like

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var url = require('url');
+var events = require('events');
 var isValidInput = require('./lib/helper').isValidInput;
 
 function DcardJS() {
@@ -12,7 +13,7 @@ function DcardJS() {
  * Get Dcard Posts ID by forum name and forum page number
  * @param {String} forum name
  * @param {Number} forum page number
- * @return {Number} post ID Number
+ * @return {Number} Post ID Number for given page number
  */
 DcardJS.prototype.getPostIdByForum = function(forumName, pageNum, callback) {
 
@@ -23,14 +24,9 @@ DcardJS.prototype.getPostIdByForum = function(forumName, pageNum, callback) {
 
   var forumAPI = url.resolve(this.FORUM_API, forumName);
 
-  for (var i = 1; i <= pageNum; i++) {
-    var forumPage = forumAPI + '/' + i.toString();
-    request(forumPage, correspondPosts);
+  var forumPage = forumAPI + '/' + pageNum.toString();
+  request(forumPage, correspondPosts);
 
-    //console.log('Sent request to -> ' + forumPage);
-  }
-
-  // console.log(forumAPI);
   function correspondPosts(error, response, body) {
     if (typeof response !== 'undefined' && !error && response.statusCode == 200) {
       if (body === '[]') {
@@ -38,13 +34,17 @@ DcardJS.prototype.getPostIdByForum = function(forumName, pageNum, callback) {
         callback(new Error('Page not found'), {});
       }
 
+      var sortedPostId = [];
       var postJson = JSON.parse(body);
-      for (var j = 0, postLen = postJson.length; j < postLen; j++) {
-        var postID = postJson[j].id;
-        callback(null, postID);
+      (function syncPostID(x) {
+        if (x < postJson.length) {
+          var postID = postJson[x].id;
+          sortedPostId.push(parseInt(postID));
+          syncPostID(x + 1);
+        }
+      })(0);
 
-        //console.log(postJson[j].id);
-      }
+      callback(null, {pageNum: pageNum, postIdList: sortedPostId});
     }
   }
 };
@@ -53,7 +53,7 @@ DcardJS.prototype.getPostIdByForum = function(forumName, pageNum, callback) {
  * Get Dcard Hot Posts ID by forum name and forum page number
  * @param {String} forum name
  * @param {Number} forum page number
- * @return {Number} post ID Number
+ * @return {Array} post ID array Number for given page number
  */
 DcardJS.prototype.getHotPostIdByForum = function(forumName, pageNum, callback) {
 
@@ -65,11 +65,7 @@ DcardJS.prototype.getHotPostIdByForum = function(forumName, pageNum, callback) {
   var hotPath = forumName + '/' + pageNum.toString() + '/popular';
   var hotPostAPI = url.resolve(this.FORUM_API, hotPath);
 
-  for (var i = 1; i <= pageNum; i++) {
-    request(hotPostAPI, correspondPosts);
-
-    //console.log('Sent request to -> ' + forumPage);
-  }
+  request(hotPostAPI, correspondPosts);
 
   // console.log(forumAPI);
   function correspondPosts(error, response, body) {
@@ -90,7 +86,7 @@ DcardJS.prototype.getHotPostIdByForum = function(forumName, pageNum, callback) {
         }
       })(0);
 
-      callback(null, {postIdList: sortedPostId});
+      callback(null, {pageNum: pageNum, postIdList: sortedPostId});
     }
   }
 };
@@ -119,9 +115,98 @@ DcardJS.prototype.getContentByPostID = function(postID, callback) {
       callback(null, {title: contentJson.version[0].title,
                       content: contentJson.version[0].content,
                       comment: contentJson.comment,
-                      url: postURL});
+                      url: postURL,
+                      fullObj: contentJson});
     }
   });
+};
+
+/**
+ * Get Dcard Posts by given page range and forum.
+ * @param {Number} start page
+ * @param {Number} end page
+ * @param {String} forum name
+ * @param {String} getType, NORMAL, HOT, HOT_WITH_FORUM
+ * @return {Array} Post object array on ascending order of time post created.
+ */
+DcardJS.prototype.getPostsByPageRangeAndForum = function(start, end, forumName, getType, callback) {
+  getType = getType || 'DEFAULT';
+  var TOTAL_POST_CNT = 0;
+  var TOTAL_POST_NUM = 0;
+  var POST_PER_PAGE = 20;
+  var RANGE = end - start + 1;
+  var postList = [];
+  var d = new DcardJS();
+  var isGetPostNum = false;
+  var eventEmitter = new events.EventEmitter();
+
+  eventEmitter.on('post-cnt', plusPostCnt);
+
+  for (var i = start; i <= end; i++) {
+    switch (getType) {
+      case 'HOT_WITH_FORUM':
+        d.getHotPostIdByForum(forumName, i, getContentByID);
+        break;
+      case 'HOT':
+        d.getHotPostId(i, getContentByID);
+        break;
+      default:
+        d.getPostIdByForum(forumName, i, getContentByID);
+    }
+
+  }
+
+  function getContentByID(err, pageInfo) {
+
+    var idList = pageInfo.postIdList;
+
+    // Guess the Totoal Posts.
+    if (pageInfo.pageNum === 1 && !isGetPostNum) {
+      TOTAL_POST_NUM = idList.length + (RANGE - 1) * POST_PER_PAGE;
+      isGetPostNum = true;
+    }else if (!isGetPostNum) {
+      TOTAL_POST_NUM = RANGE * POST_PER_PAGE;
+      isGetPostNum = true;
+    }
+
+    // Adjust the TOTAL_POST_NUM if meet the end page Number
+    if (pageInfo.pageNum === end && idList.length < POST_PER_PAGE) {
+      TOTAL_POST_NUM = TOTAL_POST_NUM - POST_PER_PAGE + idList.length;
+    }
+
+    // if id list is undefined, directly add 19 count, there are 20 post in a page.
+    if (typeof idList === 'undefined') {
+      TOTAL_POST_CNT = TOTAL_POST_CNT + 19;
+      eventEmitter.emit('post-cnt');
+      return;
+    }
+
+    for (var i = 0, len = idList.length; i < len; i++) {
+      d.getContentByPostID(idList[i], appendPost);
+    }
+  }
+
+  function appendPost(err, post) {
+    postList.push(post.fullObj);
+    eventEmitter.emit('post-cnt');
+  }
+
+  function plusPostCnt() {
+    TOTAL_POST_CNT++;
+    if (TOTAL_POST_CNT === TOTAL_POST_NUM) {
+      // All Post Get
+      postList.sort(function(a, b) {
+        switch (getType) {
+          case 'NORMAL':
+            return (new Date(a.version[0].createdAt) - new Date(b.version[0].createdAt));
+          default:
+            return a.likeCount - b.likeCount;
+        }
+      });
+
+      callback(null, postList);
+    }
+  }
 };
 
 /**
@@ -140,11 +225,7 @@ DcardJS.prototype.getHotPostId = function(pageNum, callback) {
   var hotPath = 'all/' + pageNum.toString() + '/popular';
   var hotPostAPI = url.resolve(this.FORUM_API, hotPath);
 
-  for (var i = 1; i <= pageNum; i++) {
-    request(hotPostAPI, correspondPosts);
-
-    // console.log('Sent request to -> ' + hotPostAPI);
-  }
+  request(hotPostAPI, correspondPosts);
 
   function correspondPosts(error, response, body) {
     if (typeof response !== 'undefined' && !error && response.statusCode == 200) {
@@ -164,7 +245,7 @@ DcardJS.prototype.getHotPostId = function(pageNum, callback) {
         }
       })(0);
 
-      callback(null, {postIdList: sortedPostId});
+      callback(null, {pageNum: pageNum, postIdList: sortedPostId});
     }
   }
 };
