7769b72ecd6193244a03759912406ad6f0f79d41
kevin940726 <kevin830726@gmail.com>
2016-03-14 01:50:03 +0800

    login method

--- src/dcard.js
+++ src/dcard.js
@@ -1,9 +1,42 @@
-import fetch from 'isomorphic-fetch';
+import originalFetch from 'isomorphic-fetch';
 
 const API_ORIGIN = `https://www.dcard.tw/api`;
 const FORUM_API = `${API_ORIGIN}/forum`;
 const POST_API = `${API_ORIGIN}/post`;
 const SEARCH_API = `${API_ORIGIN}/search`;
+const LOGIN_API = `${API_ORIGIN}/member/login`;
+
+const XSRF_TOKEN_REGEX = /XSRF-TOKEN=([\w-]+);/;
+
+const fetchWithAuth = function(originalFetch = fetch) {
+    this.headers = new Headers();
+
+    this.fetch = (url, options = { "headers": {} }) => {
+        options = {
+            ...options,
+            headers: {
+                ...options.headers,
+                "cookie": this.headers.getAll("cookie").join(';'),
+                "x-xsrf-token": this.headers.get("x-xsrf-token")
+            }
+        };
+
+        return originalFetch(url, options)
+            .then(response => {
+                const cookie = response.headers.getAll("set-cookie");
+                const xXsrfToken = cookie[0].match(XSRF_TOKEN_REGEX)[1];
+                this.headers.append("cookie", cookie.join(';'));
+                this.headers.append("x-xsrf-token", xXsrfToken);
+                return response;
+            })
+            .catch(reason => {
+                console.log(reason);
+            });
+    };
+};
+
+const fetch = new fetchWithAuth(originalFetch).fetch;
+
 
 export const getPostsFromForum = ({
     forum = "all", // forum name
@@ -39,10 +72,29 @@ export const getSearchResult = ({
     return fetch(`${SEARCH_API}?search=${query}&forum_alias=${forumAlias}&school=${school}`)
         .then(response => response.json())
         .catch(reason => {
-            console.log(reason);
+            console.error(reason);
         });
 };
 
-getPostsFromForum({}).then(posts => {
-    console.log(posts[0].length);
-})
+export const login = ({
+    user = "",
+    password = ""
+}) => {
+    return fetch(LOGIN_API)
+    .then(() =>
+        fetch(LOGIN_API, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json;charset=UTF-8"
+            },
+            body: JSON.stringify({ user, password })
+        })
+    )
+    .then(response => {
+        console.log(response);
+        return response;
+    })
+    .catch(reason => {
+        console.error(reason);
+    });
+};
