4d99b4d4a6cf7c0f5cca8efbcb45f4983c19b24c
kevin940726 <kevin830726@gmail.com>
2016-03-14 14:01:12 +0800

    add status, dcard, notification, notifications api

--- src/dcard.js
+++ src/dcard.js
@@ -1,15 +1,11 @@
 import originalFetch from 'isomorphic-fetch';
 
 const API_ORIGIN = `https://www.dcard.tw/api`;
-const FORUM_API = `${API_ORIGIN}/forum`;
-const POST_API = `${API_ORIGIN}/post`;
-const SEARCH_API = `${API_ORIGIN}/search`;
-const LOGIN_API = `${API_ORIGIN}/member/login`;
+const MEMBER_API = `${API_ORIGIN}/member`;
 
-const XSRF_TOKEN_REGEX = /XSRF-TOKEN=([\w-]+);/;
-
-const fetchWithAuth = function(originalFetch = fetch) {
+const DcardClient = function(originalFetch = originalFetch) {
     this.headers = new Headers();
+    const XSRF_TOKEN_REGEX = /XSRF-TOKEN=([\w-]+);/;
 
     this.fetch = (url, options = { "headers": {} }) => {
         options = {
@@ -18,14 +14,15 @@ const fetchWithAuth = function(originalFetch = fetch) {
                 ...options.headers,
                 "cookie": this.headers.getAll("cookie").join(';'),
                 "x-xsrf-token": this.headers.get("x-xsrf-token")
-            }
+            },
+            "credentials": "include"
         };
 
         return originalFetch(url, options)
             .then(response => {
-                const cookie = response.headers.getAll("set-cookie");
-                const xXsrfToken = cookie[0].match(XSRF_TOKEN_REGEX)[1];
-                this.headers.append("cookie", cookie.join(';'));
+                const cookie = response.headers.getAll("set-cookie").join(';');
+                const xXsrfToken = cookie.match(XSRF_TOKEN_REGEX)[1];
+                this.headers.append("cookie", cookie);
                 this.headers.append("x-xsrf-token", xXsrfToken);
                 return response;
             })
@@ -35,23 +32,28 @@ const fetchWithAuth = function(originalFetch = fetch) {
     };
 };
 
-const fetch = new fetchWithAuth(originalFetch).fetch;
+export const fetchWithAuth = new DcardClient(originalFetch).fetch;
 
 
-export const getPostsFromForum = ({
-    forum = "all", // forum name
-    pageFrom = 1, // fetch from page
-    pageTo = 1, // fetch until page
-    orderBy = "popular" // order by "popular" or "recent"
-}) => {
+export const getPostsFromForum = (options = {}) => {
+    options = {
+        forum: "all", // forum name
+        pageFrom: 1, // fetch from page
+        pageTo: 1, // fetch until page
+        orderBy: "popular", // order by "popular" or "recent"
+        auth: false, // authentication enable or not
+        ...options
+    };
+    let { forum, pageFrom, pageTo, orderBy, auth } = options;
     pageTo = pageFrom < pageTo ? pageTo : pageFrom;
     orderBy = orderBy === "recent" ? "" : "popular";
+    const fetch = auth ? fetchWithAuth : originalFetch;
 
     const reqArray = new Array(pageTo - pageFrom + 1)
         .fill()
         .map( (cur, i) => i + pageFrom )
         .map(i =>
-            fetch(`${FORUM_API}/${forum}/${i}/${orderBy}`)
+            fetch(`${API_ORIGIN}/forum/${forum}/${i}/${orderBy}`)
                 .then(response => response.json())
         );
 
@@ -60,16 +62,19 @@ export const getPostsFromForum = ({
 };
 
 export const getPostById = (postId = "") => {
-    return fetch(`${POST_API}/${postId}`)
+    return fetch(`${API_ORIGIN}/post/${postId}`)
         .then(response => response.json());
 };
 
 export const getSearchResult = ({
     query = "", // search query
     forumAlias = "all", // search from forum
-    school = "" // search by school name
+    school = "", // search by school name
+    auth = false
 }) => {
-    return fetch(`${SEARCH_API}?search=${query}&forum_alias=${forumAlias}&school=${school}`)
+    const fetch = auth ? fetchWithAuth : originalFetch;
+
+    return fetch(`${API_ORIGIN}/search?search=${query}&forum_alias=${forumAlias}&school=${school}`)
         .then(response => response.json())
         .catch(reason => {
             console.error(reason);
@@ -78,23 +83,57 @@ export const getSearchResult = ({
 
 export const login = ({
     user = "",
-    password = ""
+    password = "",
+    auth = true
 }) => {
-    return fetch(LOGIN_API)
-    .then(() =>
-        fetch(LOGIN_API, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json;charset=UTF-8"
-            },
-            body: JSON.stringify({ user, password })
-        })
-    )
-    .then(response => {
-        console.log(response);
-        return response;
-    })
-    .catch(reason => {
-        console.error(reason);
-    });
+    const fetch = auth ? fetchWithAuth : originalFetch;
+
+    return fetch(`${MEMBER_API}/login`)
+        .then(() =>
+            fetch(`${MEMBER_API}/login`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json;charset=UTF-8"
+                },
+                body: JSON.stringify({ user, password })
+            })
+        )
+        .catch(reason => {
+            console.error(reason);
+        });
+};
+
+export const getStatus = (auth = true) => {
+    const fetch = auth ? fetchWithAuth : originalFetch;
+
+    return fetch(`${MEMBER_API}/status`)
+        .then(response => response.json());
+};
+
+export const getTodayDcard = (auth = true) => {
+    const fetch = auth ? fetchWithAuth : originalFetch;
+
+    return fetch(`${API_ORIGIN}/dcard`)
+        .then(response => response.json());
+};
+
+export const getNotification = (auth = true) => {
+    const fetch = auth ? fetchWithAuth : originalFetch;
+
+    return fetch(`${MEMBER_API}/notification`)
+        .then(response => response.json());
+};
+
+export const getNotifications = (options = {}) => {
+    options = {
+        number: 6, // display how many notifications at a time
+        lastId: "", // last display notification id to chain
+        auth: true,
+        ...options
+    };
+    const { number, lastId, auth } = options;
+    const fetch = auth ? fetchWithAuth : originalFetch;
+
+    return fetch(`${API_ORIGIN}/notifications?n=${number}&last_id=${lastId}`)
+        .then(response => response.json());
 };
