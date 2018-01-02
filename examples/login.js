c85d3b7b3d19e1966e8157034d3944a83af2284c
lockys <locky4567@gmail.com>
2018-01-02 17:06:30 +0800

    fix #13 make it works again.

--- examples/login.js
+++ examples/login.js
@@ -4,7 +4,6 @@ const api = require('../lib/dcard');
 * The Accessibiliy function here should be required to login in.
 */
 api.sessions.createSession({ email: process.env.EMAIL, password: process.env.PWD }).then((res) => {
-
   // get today's dcard of the account you login in.
   api.dcard.getDcard().then((res) => {
     console.log(res);
--- examples/post.js
+++ examples/post.js
@@ -1,18 +1,19 @@
+const api = require('../lib/dcard');
 /**
 * These functions are open API without login.
 * See docs/forumList.json to get forum alias
 */
-const postOpt = {
+const optiton = {
   forum: 'vehicle',
   popular: false,
-  before: 225687456,
+  before: 227994176,
 };
-api.posts.listPost(postOpt).then((res) => {
+
+api.posts.listPost(optiton).then((res) => {
   console.log(res);
 });
 
-const postId = 225681815;
-const options = {};
-api.comments.listComments(postId, options).then((res) => {
+const postId = 227994176;
+api.comments.listComments(postId, {}).then((res) => {
   console.log(res);
 });
